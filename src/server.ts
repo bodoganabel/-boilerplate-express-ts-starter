fac0a6ec992eb903f9cde25a436232dc39e69a9f
bodoganabel <abel.bodogan@zooshgroup.com>
2021-06-23 15:00:14 +0200

    first commit

---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+
+const app = express()
+const PORT: string|number = process.env.PORT || 5000;
+
+// Body parsing Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+try {
+  app.listen(PORT, (): void => {
+      console.log(`Connected successfully on port ${PORT}`);
+  });
+} catch (error) {
+  console.error(`Error occured: ${error.message}`);
+}
+
+
+app.get('/', (req, res) => {
+  res.send("Hello!");
+});
+
+app.get('/example', (req, res) => {
+  res.send("example");
+});
